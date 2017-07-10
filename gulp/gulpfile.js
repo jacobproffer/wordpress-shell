3e641e3865d7174627eab47bebec2a7a3e710a97
jacobproffer <jacobhardingproffer@gmail.com>
2017-07-10 14:42:54 -0400

    Update gulp

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -6,29 +6,28 @@ var gulp        = require('gulp'),
     rename      = require('gulp-rename'),
     prefix      = require('gulp-autoprefixer'),
     uglify      = require('gulp-uglify'),
-    concat      = require('gulp-concat');
-
-var scripts = [
-  'assets/js/lib/jquery-3.2.1.min.js',
-  'assets/js/lib/headroom/headroom.min.js',
-  'assets/js/lib/headroom/jQuery.headroom.js',
-  'assets/js/app.js'
-];
+    concat      = require('gulp-concat'),
+    scripts = [
+      '../assets/js/lib/jquery-3.2.1.min.js',
+      '../assets/js/lib/headroom/headroom.min.js',
+      '../assets/js/lib/headroom/jQuery.headroom.js',
+      '../assets/js/app.js'
+    ];
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass', 'js'], function() {
-    gulp.watch('assets/scss/**/*.scss', ['sass']);
-    gulp.watch('assets/js/**/*.js', ['js']);
+    gulp.watch('../assets/scss/**/*.scss', ['sass']);
+    gulp.watch('../assets/js/**/*.js', ['js']);
 });
 
 // Configure CSS tasks.
 gulp.task('sass', function () {
-  return gulp.src('assets/scss/**/*.scss')
+  return gulp.src('../assets/scss/**/*.scss')
     .pipe(sass.sync().on('error', sass.logError))
     .pipe(prefix('last 2 versions'))
     .pipe(cssmin())
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('dist/css'))
+    .pipe(gulp.dest('../dist/css'))
 });
 
 // Configure JS.
@@ -37,12 +36,7 @@ gulp.task('js', function() {
     .pipe(uglify())
     .pipe(concat('app.js'))
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('dist/js'))
-});
-
-gulp.task('watch', function () {
-  gulp.watch('dist/scss/**/*.scss', ['sass']);
-  gulp.watch('dist/js/**/*.js', ['js']);
+    .pipe(gulp.dest('../dist/js'))
 });
 
 gulp.task('default', ['sass', 'js', 'serve']);
