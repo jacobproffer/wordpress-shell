9761a2b1bed6417898799068354b76e33c8d137c
Jacob Proffer <jacobhardingproffer@gmail.com>
2018-01-19 16:04:05 -0500

    Add wp_footer to footer

--- footer.php
+++ footer.php
@@ -2,5 +2,6 @@
       <div class="container"></div>
     </footer>
     <script src="<?php echo get_stylesheet_directory_uri(); ?>/dist/js/app.min.js" charset="utf-8"></script>
+<?php wp_footer(); ?>
   </body>
 </html>
